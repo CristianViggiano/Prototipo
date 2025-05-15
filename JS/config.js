5c1fd2929795ddc6ad9d6341ff42a5549e207528
Cristian Viggiano <145474404+CristianViggiano@users.noreply.github.com>
2025-05-15 18:48:01 -0300

    Create config.js

---
+++ JS/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'sa-east-1_wYTzpSzd8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5qa8hjq6mlfp0l0g7tcp6f67ga', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
